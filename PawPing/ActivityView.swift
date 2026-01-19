e2b5012a7760eb5278911f7f4fca66c7fe0a39ac
Atul Rai <atulrai.varanasi@gmail.com>
2026-01-19 12:13:35 +0530

    Activity View Commit

---
+++ PawPing/ActivityView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityView.swift
+//  PawPing
+//
+//  Created by Atul on 19/01/26.
+//
+
+import SwiftUI
+
+struct ActivityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityView()
+}
