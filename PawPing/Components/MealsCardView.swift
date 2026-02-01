c99b26a99bef14996e0ee6f60d0ab20758acc715
Atul Rai <atulrai.varanasi@gmail.com>
2026-02-02 00:15:46 +0530

    Meals and Vaccine card added in Activities

---
+++ PawPing/Components/MealsCardView.swift
@@ -0,0 +1,18 @@
+//
+//  MealsCardView.swift
+//  PawPing
+//
+//  Created by Atul on 01/02/26.
+//
+
+import SwiftUI
+
+struct MealsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealsCardView()
+}
