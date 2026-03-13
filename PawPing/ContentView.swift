30fc44eea9e8d19e2450c46f02d5ce27b98e153e
Atul Rai <atulrai.varanasi@gmail.com>
2026-03-13 12:57:33 +0530

    fixed content view in main

--- PawPing/ContentView.swift
+++ PawPing/ContentView.swift
@@ -2,14 +2,33 @@
 //  ContentView.swift
 //  PawPing
 //
-//  Created by Atul on 13/03/26.
+//  Created by Atul on 19/01/26.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    let sampleProfile = DogProfile(id: UUID(), ownerId: UUID(), dogName: "Buddy", breed: "Labrador", gender: "male", age: "2")
+    let sampleActivity = WalkActivity(currentMinutes: 23, goalMinutes: 60)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TabView {
+                // 2. Pass the sample data to ActivityView
+                Tab("Activity", systemImage: "dog.fill") {
+                    ActivityView()
+                }
+
+                Tab("Care", systemImage: "pawprint.fill") {
+                    CareView()
+                }
+
+                Tab("Vaccine", systemImage: "syringe.fill") {
+                    Text("Vaccine View")
+                }
+            }
+            .tint(Color("baseRed"))
+        }
     }
 }
 
