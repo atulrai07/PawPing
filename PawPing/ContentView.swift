168f176e200d736d349c48d7fb076f58dee81f1e
Atul Rai <atulrai.varanasi@gmail.com>
2026-03-13 12:56:18 +0530

    added cotentView in main

---
+++ PawPing/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  PawPing
+//
+//  Created by Atul on 13/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawPing/PawPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawPingApp.swift
+//  PawPing
+//
+//  Created by Atul on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PawPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
