0bc9b5a9dbbc9e0b80a24849da957533f3e63618
Shailesh Kumar <shailesh99394@gmail.com>
2026-03-19 23:32:23 +0530

    navigation title custom

---
+++ PawPing/CustomNavigationScroll.swift
@@ -0,0 +1,18 @@
+//
+//  CustomNavigationScroll.swift
+//  PawPing
+//
+//  Created by Shailesh on 19/03/26.
+//
+
+import SwiftUI
+
+struct CustomNavigationScroll: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomNavigationScroll()
+}
