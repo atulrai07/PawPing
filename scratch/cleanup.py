d1f505392f277ec27dd2f666130047f9fb2e5c34
Atul Rai <atulrai.varanasi@gmail.com>
2026-05-04 16:41:16 +0530

    Delete scratch directory

--- scratch/cleanup.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-
-path = "/Users/sidmoon/Downloads/PawPing-develop-atul/PawPing/Activity Tab/Symptom Checker"
-if os.path.exists(path):
-    shutil.rmtree(path)
-    print(f"Deleted {path}")
-else:
-    print(f"{path} does not exist")
